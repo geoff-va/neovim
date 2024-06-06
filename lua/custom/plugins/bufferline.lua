428d87f1814f3ca41f166529c06349026d7086a7
Geoff Von Allmen <geoff@ibleducation.com>
2024-06-06 12:07:47 -0700

    refactor: Use nicer bufferline options

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,1 +1,12 @@
-return { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", opts = {} }
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			separator_style = "slant",
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+		},
+	},
+}

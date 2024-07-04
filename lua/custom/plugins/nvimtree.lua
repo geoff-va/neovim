3d6fedb6e19dcf12291769ddc5b79c4f3548ffca
Geoff Von Allmen <geoff@ibleducation.com>
2024-07-03 21:32:53 -0700

    refactor: find files/grep in nvimtree folders

--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -1,7 +1,43 @@
+-- https://github.com/alex-courtis/arch/blob/39a1ba4ecc3d356bb660a489d3648b987ecd9a85/config/nvim/lua/amc/plugins/nvt.lua#L20
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+	local telescope = require("telescope.builtin")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	local function node_dir_path()
+		local node = api.tree.get_node_under_cursor()
+		if not node then
+			return
+		end
+
+		if node.parent and node.type == "file" then
+			node = node.parent
+		end
+
+		return node.absolute_path
+	end
+
+	local function find_files()
+		telescope.find_files({ search_dirs = { node_dir_path() } })
+	end
+
+	local function live_grep()
+		telescope.live_grep({ search_dirs = { node_dir_path() } })
+	end
+
+	api.config.mappings.default_on_attach(bufnr)
+	vim.keymap.set("n", "<leader>sf", find_files, opts("Find Files In Folder"))
+	vim.keymap.set("n", "<leader>sg", live_grep, opts("Live Grep Folder"))
+end
+
 return {
 	{
 		"nvim-tree/nvim-tree.lua",
 		opts = {
+			on_attach = on_attach,
 			-- nvim-project settings
 			sync_root_with_cwd = true,
 			update_focused_file = {

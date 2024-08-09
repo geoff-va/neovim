1e104b6179f3ad74a588ce153e730ab62023d8fc
Geoff Von Allmen <geoff@ibleducation.com>
2024-08-08 22:46:41 -0700

    refactor: Add go.nvim; disabled
    - Seems to override all my lsp stuff which I don't like

---
+++ lua/custom/plugins/go-nvim.lua
@@ -0,0 +1,18 @@
+return {
+	"ray-x/go.nvim",
+	dependencies = { -- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	enabled = false,
+	config = function()
+		require("go").setup({})
+		require("lspconfig").gopls.setup({})
+		-- local cfg = require("go.lsp").config() -- config() return the go.nvim gopls setup
+		-- require("lspconfig").gopls.setup(cfg)
+	end,
+	event = { "CmdlineEnter" },
+	ft = { "go", "gomod" },
+	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+}

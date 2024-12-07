c5cd3f881990c70cf96339adb89a3a173a0134e8
Geoff Von Allmen <geoff@ibleducation.com>
2024-12-07 11:23:09 -0800

    refactor: disable cmp-git; not sure what it's doing?

--- lua/custom/plugins/cmp-git.lua
+++ lua/custom/plugins/cmp-git.lua
@@ -1,6 +1,11 @@
 return {
 	"petertriho/cmp-git",
+	dependencies = { "hrsh7th/nvim-cmp" },
+	enabled = false,
 	opts = {
-		name = "git",
+		-- options go here
 	},
+	init = function()
+		table.insert(require("cmp").get_config().sources, { name = "git" })
+	end,
 }
